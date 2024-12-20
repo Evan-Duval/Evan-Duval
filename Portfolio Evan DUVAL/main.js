f50e5faaf5f85015b130666422bf7abb592befe4
Emix4 <emix838@gmail.com>
2024-12-20 21:10:56 +0100

    wip, remove useless media

--- Portfolio Evan DUVAL/main.js
+++ Portfolio Evan DUVAL/main.js
@@ -3,14 +3,18 @@ const navbarContainer = document.getElementById('navbar');
 fetch('../auto/navbar/navbar.html')
     .then(response => response.text())
     .then(data => {
-        navbarContainer.innerHTML = data;
+        // Insérer la navbar dans le conteneur
+        document.getElementById('navbar').innerHTML = data;
 
-        const navItems = document.querySelectorAll('.nav-links ul li');
-        navItems.forEach(item => {
-            item.addEventListener('click', () => {
-                navItems.forEach(nav => nav.classList.remove('active'));
-                item.classList.add('active');
-            });
+        // Identifier la page actuelle à partir de l'URL
+        const currentPage = window.location.pathname.split('/')[1]; // Extrait le dossier courant
+        const navLinks = document.querySelectorAll('.nav-links ul li a');
+
+        // Parcourir tous les liens pour marquer le lien actif
+        navLinks.forEach(link => {
+            if (link.getAttribute('data-page') === currentPage) {
+                link.parentElement.classList.add('active');
+            }
         });
     })
     .catch(error => console.error('Erreur lors du chargement de la navbar:', error));
