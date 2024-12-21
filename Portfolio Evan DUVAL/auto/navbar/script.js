113ff41e80ac0f205882efa2b880de77da56258a
Emix4 <emix838@gmail.com>
2024-12-21 10:41:55 +0100

    backup

--- Portfolio Evan DUVAL/auto/navbar/script.js
+++ Portfolio Evan DUVAL/auto/navbar/script.js
@@ -4,11 +4,3 @@ const navLinks = document.querySelector('.nav-links'); // Modifié pour sélecti
 navExtender.addEventListener('click', () => {
     navLinks.classList.toggle('visible'); // Utilisez toggle au lieu de add
 });
-
-const navItems = document.querySelectorAll('.nav-links ul li');
-navItems.forEach(item => {
-    item.addEventListener('click', () => {
-        navItems.forEach(nav => nav.classList.remove('active'));
-        item.classList.add('active');
-    });
-});
