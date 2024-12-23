2a62a819422a555d29956f92807e60ed2c40f9e3
Evan-Duval <duvalevan368@gmail.com>
2024-12-23 19:37:06 +0100

    moove portfolio into a private repo linked with ovh by FTP

--- Portfolio Evan DUVAL/auto/navbar/script.js
+++
@@ -1,6 +0,0 @@
-const navExtender = document.querySelector('.nav-extender');
-const navLinks = document.querySelector('.nav-links'); // Modifié pour sélectionner un seul élément
-
-navExtender.addEventListener('click', () => {
-    navLinks.classList.toggle('visible'); // Utilisez toggle au lieu de add
-});
